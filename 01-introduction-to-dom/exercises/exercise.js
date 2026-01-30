68edba92e5dbb01c004636efae31fbb302f24879
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-01-30 09:15:49 -0500

    Completed 01-introduction-to-dom

--- 01-introduction-to-dom/exercises/exercise.js
+++ 01-introduction-to-dom/exercises/exercise.js
@@ -9,34 +9,37 @@ console.log(document);
 
 // 2. Log the page title to the console
 // Your code here:
-
+console.log(document.title);
 
 // 3. Log the current URL of the page to the console
 // Your code here:
-
+console.log(document.URL);
 
 // 4. Log the <body> element to the console
 // Your code here:
-
+console.log(document.body);
 
 // 5. Log the <head> element to the console
 // Your code here:
-
+console.log(document.head);
 
 // 6. Log the <html> element (documentElement) to the console
 // Your code here:
-
+console.log(document.documentElement);
 
 // 7. Log the character set of the document
 // Hint: use document.characterSet
 // Your code here:
+console.log(document.characterSet);
 
 
 // 8. Log the content type of the document
 // Hint: use document.contentType
 // Your code here:
-
+console.log(document.contentType);
 
 // BONUS: Try changing the document title and observe what happens
 // Your code here:
+document.title = "CHANGED TITLE"
+console.log("New Title:", document.title);
 
